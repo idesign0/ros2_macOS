0ddeeaab2f38045204daeb333dd83aefcca98849
Dhruv Patel <dhruvpatel2991998@gmail.com>
2026-01-05 11:38:23 +0100

    CI: CI_BUILD logic fix

--- cmake/toolchain.cmake
+++ cmake/toolchain.cmake
@@ -1,7 +1,7 @@
 message(WARN " Toolchain.cmake is being used.")
 
 # --- Detect if we are running in CI or local ---
-if(DEFINED ENV{CI_BUILD} AND "$ENV{CI_BUILD}" STREQUAL "TRUE")
+if(CI_BUILD OR "$ENV{CI_BUILD}" STREQUAL "TRUE")
     set(IS_CI TRUE)
     message(STATUS "CI environment detected")
 else()
